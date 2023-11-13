678ddcffb90e7177d4b756e93a84362f9f0a1a87
CesarPdev <cesarpedetta@gmail.com>
2023-11-13 08:37:45 -0300

    changing colors

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
 
   return (
     <>
-      <div className='flex flex-col h-screen items-center justify-center'>
-        <h1 className='text-3xl m-4'>Hello World!</h1>
+      <div className='flex flex-col h-screen bg-gradient-to-t from-violet-500 to-fuchsia-500 items-center justify-center'>
+        <h1 className='text-3xl m-4'>Your next fly!</h1>
         <button
-          className='border p-2 rounded-md'
+          className='bg-cyan-500 shadow-cyan-500/50 p-2 rounded-md shadow-lg'
           onClick={() => setCount((count) => count + 1)}
         >
           count is {count}
